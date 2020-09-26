f1b5462dd3594f89d5cad4c0ea535fe2985cadf4
aweisser <armin.weisser@gmail.com>
2020-09-26 17:29:40 +0200

    Dynamically set next contrib meeting links

--- docs/assets/js/dscss.js
+++ docs/assets/js/dscss.js
@@ -1,9 +1,3 @@
-function addToDiscussions(dscssn) {
-    if(discussionIsJoinable(dscssn)) {
-        $('.search-results').append(discussionResultItem(dscssn));
-    }
-}
-
 function discussionIsJoinable(dscssn) {
     return discussionEndIsInTheFuture(dscssn);
 }
@@ -85,3 +79,34 @@ function saveDiscussionDateLink(dscssn) {
         </div>
     </div>`;
 }
+
+function nextContribMeetingHtml(dscssn) {
+    var cal = createCalendar({
+        data: {
+            producer: 'dscss.online',
+            title: dscssn.title,
+            start: new Date(dscssn.start_time.seconds * 1000),
+            duration: dscssn.duration_min,
+            location: dscssn.meeting_url,
+            description: dscssn.description 
+            + "\n\n\n"
+            + "Click here to join the meeting:"
+            + "\n"
+            + dscssn.meeting_url
+        }
+    });
+    
+    return `
+        <p class="text-center">
+            Next meetup: ${dayjs.unix(dscssn.start_time.seconds).format('MMMM D, YYYY h:mm A')}
+        </p>
+        <p class="text-center">
+            <a href="${cal.ical}" title="Apple"><img src="assets/images/apple.svg" alt="Apple" style="width:23px;;margin-left:11px;margin-right:11px;padding-bottom:5px;"/></a>
+            <a href="${cal.google}" title="Google" target="_blank"><img src="assets/images/google.svg" alt="Google" style="width:25px;margin-left:10px;margin-right:10px;" /></a>
+            <a href="${cal.office365}" title="Office 365" target="_blank"><img src="assets/images/office-365.svg" alt="Office 365" style="width:19px;;margin-left:13px;margin-right:13px;" /></a>
+            <a href="${cal.outlook}" title="Outlook"><img src="assets/images/outlook.svg" alt="Outlook" style="width:31px;;margin-left:6px;margin-right:6px;" /></a>
+            <a href="${cal.outlookCom}" title="Outlook.com" target="_blank"><img src="assets/images/outlookCom.svg" alt="Outlook.com" style="width:31px;;margin-left:6px;margin-right:6px;" /></a>
+            <a href="${cal.yahoo}" title="Yahoo" target="_blank"><img src="assets/images/yahoo.svg" alt="Yahoo" style="width:25px;;margin-left:10px;margin-right:10px;" /></a>
+        </p>`;
+}
+
